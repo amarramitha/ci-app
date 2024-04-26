ba3e637eec6980d3000a3790b9e4a6d72e2e21da
amarra <amarra.mitha@gmail.com>
2024-04-26 15:45:37 +0700

    1

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -13,15 +13,12 @@
   <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-  <a class="navbar-brand" href="#">Data Unsoed</a>
+  <a class="navbar-brand">Data Unsoed</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="<?= base_url(); ?>">Home <span class="sr-only">(current)</span></a>
-      </li>
       <li class="nav-item">
         <a class="nav-link" href="<?= base_url('mahasiswa'); ?>">Mahasiswa</a>
       </li>
